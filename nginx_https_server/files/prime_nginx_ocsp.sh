dd8f3492d004f570f938e395a134b67865e9b550
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-09-24 18:03:59 +0200

    use openssl to make the request to prime the ocsp cache, fetch doesn't work

--- nginx_https_server/files/prime_nginx_ocsp.sh
+++ nginx_https_server/files/prime_nginx_ocsp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for vhost in $(ls -1 /usr/local/etc/nginx/vhosts/*https.conf | rev | cut -d "/" -f 1 | rev | sed "s/-https.conf$//"); do
-        /usr/bin/fetch -qo /dev/null https://${vhost}/__nginx_ocsp_priming/ 2>/dev/null
+        /usr/local/bin/openssl s_client -connect ${vhost}:443 -servername ${vhost} -status < /dev/null 2>&1
 done
