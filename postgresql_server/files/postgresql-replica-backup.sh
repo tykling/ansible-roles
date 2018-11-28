95d55e7634c9a07275b1d544eee3d5a33cd87558
Thomas Steen Rasmussen <thomas@gibfest.dk>
2018-11-28 07:44:06 +0100

    pg_xlog_* -> pg_wal_*

--- postgresql_server/files/postgresql-replica-backup.sh
+++ postgresql_server/files/postgresql-replica-backup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # pause WAL streaming replication playback
-/usr/local/bin/psql -c "SELECT pg_xlog_replay_pause();"
+/usr/local/bin/psql -c "SELECT pg_wal_replay_pause();"
 
 if [ $? -ne 0 ]; then
-	echo "unable to pause WAL playback, bailing out"
-	exit 1
+    echo "unable to pause WAL playback, bailing out"
+    exit 1
 fi
 
 # do the backup by running the daily periodic script
 daily_pgsql_backup_enable=YES /usr/local/etc/periodic/daily/502.pgsql
 
 # resume WAL streaming replication playback
-/usr/local/bin/psql -c "SELECT pg_xlog_replay_resume();"
+/usr/local/bin/psql -c "SELECT pg_wal_replay_resume();"
 
