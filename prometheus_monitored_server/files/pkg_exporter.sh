0a168c414e4560991e3d73f6bfcdcd61ee290800
Thomas Steen Rasmussen <thomas@gibfest.dk>
2020-06-01 02:00:56 +0200

    fixup pkg_exporter.sh a bit

--- prometheus_monitored_server/files/pkg_exporter.sh
+++ prometheus_monitored_server/files/pkg_exporter.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 /usr/sbin/pkg update
-/bin/echo "pkg_info{version=\"$(pkg -v)\", abi=\"$(/usr/sbin/pkg config abi)\"} 1"
+
+echo "# HELP pkg_build_info The version of pkg installed"
+echo "# TYPE pkg_build_info gauge"
+/bin/echo "pkg_build_info{version=\"$(/usr/sbin/pkg -v)\", abi=\"$(/usr/sbin/pkg config abi)\"} 1"
+
+echo "# HELP pkg_packages_total Total number of pkg packages grouped by version as compared to index."
+echo "# TYPE pkg_packages_total gauge"
 /bin/echo "pkg_packages_total{version=\"equal\"} $(/usr/sbin/pkg version -l \= | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
 /bin/echo "pkg_packages_total{version=\"older\"} $(/usr/sbin/pkg version -l \< | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
 /bin/echo "pkg_packages_total{version=\"newer\"} $(/usr/sbin/pkg version -l \> | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
+
+echo "# HELP pkg_vulnerable_packages_total Total number of vulnerable packages installed, as reported by 'pkg audit'."
+echo "# TYPE pkg_vulnerable_packages_total gauge"
 /bin/echo "pkg_vulnerable_packages_total{version=\"vulnerable\"} $(/usr/sbin/pkg audit -q | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
 
