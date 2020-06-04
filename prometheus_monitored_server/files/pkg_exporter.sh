d7fc190feddbcdd02ea074e291b68d6d14f847f5
Thomas Steen Rasmussen <thomas@gibfest.dk>
2020-06-04 23:12:35 +0200

    add -q to suppress debug output which mysteriously only shows up when running as root

--- prometheus_monitored_server/files/pkg_exporter.sh
+++ prometheus_monitored_server/files/pkg_exporter.sh
@@ -7,9 +7,9 @@ echo "# TYPE pkg_build_info gauge"
 
 echo "# HELP pkg_packages_total Total number of pkg packages grouped by version as compared to index."
 echo "# TYPE pkg_packages_total gauge"
-/bin/echo "pkg_packages_total{version=\"equal\"} $(/usr/sbin/pkg version -l \= | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
-/bin/echo "pkg_packages_total{version=\"older\"} $(/usr/sbin/pkg version -l \< | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
-/bin/echo "pkg_packages_total{version=\"newer\"} $(/usr/sbin/pkg version -l \> | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
+/bin/echo "pkg_packages_total{version=\"equal\"} $(/usr/sbin/pkg version -ql \= | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
+/bin/echo "pkg_packages_total{version=\"older\"} $(/usr/sbin/pkg version -ql \< | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
+/bin/echo "pkg_packages_total{version=\"newer\"} $(/usr/sbin/pkg version -ql \> | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
 
 echo "# HELP pkg_vulnerable_packages_total Total number of vulnerable packages installed, as reported by 'pkg audit'."
 echo "# TYPE pkg_vulnerable_packages_total gauge"
