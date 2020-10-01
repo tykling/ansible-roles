e5907b8e4d7c38fe70f2b9c1fd4f32bb4036ee5d
Thomas Steen Rasmussen <thomas@gibfest.dk>
2020-10-01 13:28:37 +0200

    also work the first 9 days of the month, geez

--- bind_server/files/bind_dampening_stats.sh
+++ bind_server/files/bind_dampening_stats.sh
@@ -13,8 +13,8 @@ fi
 
 # tail log and add as we go
 tail -F /var/log/bind-dampening.log | grep --line-buffered "Stats for" | while read -r line; do
-   view=$(echo "$line" | cut -d " " -f 8 | cut -d "#" -f 1)
-   damps=$(echo "$line" | cut -d " " -f 10)
+   view=$(echo "$line" | sed "s/  / /" | cut -d " " -f 8 | cut -d "#" -f 1)
+   damps=$(echo "$line" | sed "s/  / /" | cut -d " " -f 10)
    grep -v "$view" "$PROMFILE" > "$TEMPFILE"
    echo "bind_dampened_total{view=\"${view}\"} $damps" >> "$TEMPFILE"
    chmod 0644 "$TEMPFILE"
