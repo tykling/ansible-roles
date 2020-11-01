e629016b5240f6a068f389f313051d5b6fda20a3
Thomas Steen Rasmussen <thomas@gibfest.dk>
2020-11-01 22:56:20 +0100

    clean up lte_modem_exporter.sh

--- freebsd_host/files/lte_modem_exporter.sh
+++ freebsd_host/files/lte_modem_exporter.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # lte_modem_exporter.sh uses pipeserial to get modem info and signal strength
+
+# output lte_modem_rssi
+echo "# HELP lte_modem_rssi The current signal strength of the modem as returned by AT+CSQ."
+echo "# TYPE lte_modem_rssi gauge"
+
+# loop over modems and output rssi for each
 for modem in "$@"; do
     # run ATI
     ATI=$(echo "ATI" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | tail +2)
@@ -7,20 +13,12 @@ for modem in "$@"; do
     model=$(echo "$ATI" | head -2 | tail -1 | sed "s/^Model: //")
     rev=$(echo "$ATI" | head -3 | tail -1 | sed "s/^Revision: //")
 
+    # run AT+GSN
+    serial=$(echo "AT+GSN" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | tail +2 | head -1)
+
     # run AT+CSQ
     CSQ=$(echo "AT+CSQ" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep "^+CSQ")
     rssi=$(echo "$CSQ" | cut -d " " -f 2 | cut -d "," -f 1)
 
-    # run AT+GSN
-    serial=$(echo "AT+GSN" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | tail +2 | head -1)
-
-    # output lte_modem_up
-    echo "# HELP lte_modem_up This metric is always 1"
-    echo "# TYPE lte_modem_up gauge"
-    /bin/echo "lte_modem_up{device=\"$modem\", manufacturer=\"$manu\", model=\"$model\", revision=\"$rev\", serial=\"$serial\"} 1"
-
-    # output lte_modem_rssi
-    echo "# HELP lte_modem_rssi The current signal strength of the modem as returned by AT+CSQ."
-    echo "# TYPE lte_modem_rssi gauge"
     /bin/echo "lte_modem_rssi{device=\"$modem\", manufacturer=\"$manu\", model=\"$model\", revision=\"$rev\", serial=\"$serial\"} $rssi"
 done
