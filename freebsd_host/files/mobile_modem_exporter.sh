4d90a37116c769fb151ebcb5f8602f4e93ae01b1
Thomas Steen Rasmussen <thomas@gibfest.dk>
2020-11-03 17:49:26 +0100

    trim empty lines before parsing response

--- freebsd_host/files/mobile_modem_exporter.sh
+++ freebsd_host/files/mobile_modem_exporter.sh
@@ -8,7 +8,7 @@ echo "# TYPE mobile_modem_rssi gauge"
 # loop over modems and output rssi for each
 for modem in "$@"; do
     # run ATI
-    ATI=$(echo "ATI" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep -v "^\^" | tail +2)
+    ATI=$(echo "ATI" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep -v "^\^" | grep -v "^$" | tail +2)
 
     # manufacturer, model, revision is (hopefully?) the three first lines
     manu=$(echo "$ATI" | head -1 |  sed "s/^Manufacturer: //")
@@ -16,10 +16,10 @@ for modem in "$@"; do
     rev=$(echo "$ATI" | head -3 | tail -1 | sed "s/^Revision: //")
 
     # run AT+GSN
-    serial=$(echo "AT+GSN" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep -v "^\^" | tail +2 | head -1)
+    serial=$(echo "AT+GSN" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep -v "^\^" | grep -v "^$" | tail +2 | head -1)
 
     # run AT+CSQ
-    CSQ=$(echo "AT+CSQ" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep -v "^\^" | grep "^+CSQ")
+    CSQ=$(echo "AT+CSQ" | /usr/local/pipeserial_venv/bin/pipeserial "$modem" | grep -v "^\^" | grep -v "^$" | grep "^+CSQ")
     rssi=$(echo "$CSQ" | cut -d " " -f 2 | cut -d "," -f 1)
     # BER is not supported on some modems, value is always 99
     ber=$(echo "$CSQ" | cut -d " " -f 2 | cut -d "," -f 2)
