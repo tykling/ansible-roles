e9fc5b1665ae9bdf72671987d43366581b1fe99a
Thomas Steen Rasmussen <thomas@gibfest.dk>
2020-12-20 15:37:04 +0100

    pkg_exporter: add a new metric pkg_packages_freebsd_version_total with a label with the FreeBSD_version annotation from pkg. Use to alert on packages from different __Freebsd_version versions

--- freebsd_base/files/pkg_exporter.sh
+++ freebsd_base/files/pkg_exporter.sh
@@ -15,3 +15,8 @@ echo "# HELP pkg_vulnerable_packages_total Total number of vulnerable packages i
 echo "# TYPE pkg_vulnerable_packages_total gauge"
 /bin/echo "pkg_vulnerable_packages_total $(/usr/sbin/pkg audit -q | /usr/bin/wc -l | /usr/bin/awk '{print $1}')"
 
+echo "# HELP pkg_packages_freebsd_version_total Total number of installed packages labeled with the FreeBSD_version annotation."
+echo "# TYPE pkg_packages_freebsd_version_total gauge"
+for version in "$(/usr/sbin/pkg annotate --all --show FreeBSD_version | /usr/bin/cut -d " " -f 5 | /usr/bin/sort | /usr/bin/uniq -c | /usr/bin/awk '{print "pkg_packages_freebsd_version_total{freebsd_version=\x22" $2 "\x22} " $1}')"; do
+        /bin/echo "$version";
+done
