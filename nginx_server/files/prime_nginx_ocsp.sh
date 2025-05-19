d68f1d4ba8bb09a5fec9ba1339f7478c982b52da
Thomas Steen Rasmussen <thomas@gibfest.dk>
2025-05-19 13:59:37 +0200

    nginx_server: remove ocsp stapling from configs

--- nginx_server/files/prime_nginx_ocsp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for vhost in $(find /usr/local/etc/nginx/vhosts/ -type f -name '*https.conf' | rev | cut -d "/" -f 1 | rev | sed "s/-https.conf$//"); do
-        echo -e "GET /__nginx_ocsp_priming/ HTTP/1.1\r\nhost: ${vhost}\r\n" | /usr/local/bin/openssl s_client -connect ${vhost}:443 -servername ${vhost} -status > /dev/null 2>&1
-done
